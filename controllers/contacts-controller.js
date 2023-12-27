0aa1b7b873ab02eb7e7e1e6a3649e20788857f7a
Oleksii81 <tourus81@gmail.com>
2023-12-27 15:54:00 +0200

    Update contacts-controller.js

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -35,7 +35,7 @@ const add = async (req, res, next) => {
 
 const updateById = async (req, res, next) => {
         const { id } = req.params;
-        const result = await Contact.findByIdAndUpdate(id, req.body);
+        const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
         if (!result) {
             throw HttpError(404, `Contact with id=${id} not found`);
         }
